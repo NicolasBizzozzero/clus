26f838e13ef8afdb917686e94ac8897b5424311c
NicolasBizzozzero <nicolasbizzozzero@gmail.com>
2019-03-22 12:57:11 +0100

    Exported all clustering algorithms

--- clus/__init__.py
+++ clus/__init__.py
@@ -1,6 +1,15 @@
 from clus.src.methods import kmeans
+from clus.src.methods import fuzzy_c_means
+from clus.src.methods import fuzzy_c_medoids
+from clus.src.methods import linearized_fuzzy_c_medoids
 
-__all__ = ["kmeans"]
+
+__all__ = [
+    "kmeans",
+    "fuzzy_c_means",
+    "fuzzy_c_medoids",
+    "linearized_fuzzy_c_medoids"
+]
 
 
 if __name__ == "__main__":
