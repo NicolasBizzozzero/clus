1fd2b5bebae10ac755a9f4025375e967131dd0fc
NicolasBizzozzero <nicolasbizzozzero@gmail.com>
2019-04-09 16:10:47 +0200

    Faster and memory-efficient fcm

--- clus/src/core/methods/fuzzy_c_means.py
+++ clus/src/core/methods/fuzzy_c_means.py
@@ -137,7 +137,7 @@ def __compute_memberships(data, centroids, fuzzifier):
                     u_ir[i][s] = 0
                 u_ir[i][r] = 1
                 break
-            big_sum = sum((d_ir / (np.linalg.norm(data[i] - centroids[s], ord=2) ** 2)) ** (2 / (fuzzifier - 1))
+            big_sum = sum((d_ir / (np.linalg.norm(data[i] - centroids[s], ord=2))) ** (2 / (fuzzifier - 1))
                           for s in range(centroids.shape[0]))
             u_ir[i][r] = 1 / big_sum
     return u_ir
@@ -151,7 +151,7 @@ def __compute_loss(data, memberships, centroids, fuzzifier):
     for i in range(centroids.shape[0]):
         for j in range(data.shape[0]):
             membership_fuzzified = memberships[j][i] ** fuzzifier
-            dist_data_centroid = np.linalg.norm(data[j] - centroids[i], ord=2) ** 2
+            dist_data_centroid = np.linalg.norm(data[j] - centroids[i], ord=2)
             res += membership_fuzzified * dist_data_centroid
     return res
 
