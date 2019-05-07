36cab57fa71031499310c797bdfdf02f0820f620
NicolasBi <nicolasbizzozzero@gmail.com>
2019-05-07 18:19:14 +0200

    dzhvdzaudguaydvudazb

--- clus/src/utils/path.py
+++ clus/src/utils/path.py
@@ -12,12 +12,37 @@ def compute_file_saving_path(dataset, clustering_algorithm, components, seed, di
     return os.path.join(dir_dest,
                         "{dataset}_{clustering_algorithm}_{components}_{seed}_{distance}{weights}{fuzzifier}"
                         "{is_3d_visualisation}.{extension}".format(
-                            dataset=os.path.splitext(ntpath.basename(dataset))[0],
+                            dataset=os.path.splitext(
+                                ntpath.basename(dataset))[0],
                             clustering_algorithm=clustering_algorithm,
                             components=components,
                             distance=distance,
-                            weights=("_(" + '-'.join(weights) + ")") if distance == "weighted_euclidean" else "",
-                            fuzzifier=("_" + str(fuzzifier)) if fuzzifier is not None else "",
+                            weights=(
+                                "_(" + '-'.join(weights) + ")") if distance == "weighted_euclidean" else "",
+                            fuzzifier=("_" + str(fuzzifier)
+                                       ) if fuzzifier is not None else "",
+                            seed=seed,
+                            is_3d_visualisation="_3d" if is_3d_visualisation else "",
+                            extension=extension
+                        ))
+
+
+def compute_file_saving_path_dclus(dataset, clustering_algorithm, seed, distance, weights, dir_dest,
+                                   extension, is_3d_visualisation=False):
+    os.makedirs(dir_dest, exist_ok=True)
+
+    if weights is not None:
+        weights = tuple(map(lambda n: str(n), weights))
+
+    return os.path.join(dir_dest,
+                        "{dataset}_{clustering_algorithm}_{seed}_{distance}{weights}"
+                        "{is_3d_visualisation}.{extension}".format(
+                            dataset=os.path.splitext(
+                                ntpath.basename(dataset))[0],
+                            clustering_algorithm=clustering_algorithm,
+                            distance=distance,
+                            weights=(
+                                "_(" + '-'.join(weights) + ")") if distance == "weighted_euclidean" else "",
                             seed=seed,
                             is_3d_visualisation="_3d" if is_3d_visualisation else "",
                             extension=extension
